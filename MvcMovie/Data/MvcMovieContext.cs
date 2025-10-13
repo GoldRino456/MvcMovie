68de77c41e277c2e5d67b3b65537946dde105ce3
Ethan E. <eastwoode375@gmail.com>
2025-10-13 16:04:00 -0500

    Style Clean-Up

--- MvcMovie/Data/MvcMovieContext.cs
+++ MvcMovie/Data/MvcMovieContext.cs
@@ -1,19 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using MvcMovie.Models;
 
-namespace MvcMovie.Data
+namespace MvcMovie.Data;
+
+public class MvcMovieContext : DbContext
 {
-    public class MvcMovieContext : DbContext
+    public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+        : base(options)
     {
-        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
     }
+
+    public DbSet<Models.Movie> Movie { get; set; } = default!;
 }
--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -1,12 +1,11 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using MvcMovie.Data;
 using MvcMovie.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MvcMovieContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext") ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -17,11 +16,9 @@ using (var scope = app.Services.CreateScope())
     SeedData.Initialize(services);
 }
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -34,7 +31,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
         name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}")
+        pattern: "{controller=Movies}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
